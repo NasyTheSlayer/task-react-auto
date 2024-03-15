25d7d9b081298d27886f69789760118482a6a1e5
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-03-15 13:16:56 +0200

    fix urlv1.4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/task-react-auto/",
+  base: "/task-react-auto",
   plugins: [react()],
 });
